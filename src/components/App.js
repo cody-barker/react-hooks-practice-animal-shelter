0661eb0e4e382c4dbb16167dfb9eae31f4ce182c
Cody <codybarker.or@gmail.com>
2023-03-06 14:23:41 -0800

    all deliverables met

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,34 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onChangeType(e) {
+    setFilters({type: e.target.value})
+  }
+
+  function onFindPetsClick(e) {
+    if (filters.type === "all") {
+    fetch(`http://localhost:3001/pets`)
+    .then(r => r.json())
+    .then(list => setPets(list))
+    } else {
+    fetch(`http://localhost:3001/pets?type=${filters.type}`)
+    .then(r => r.json())
+    .then(list => setPets(list))
+    }
+  }
+
+  function onAdoptPet(pet) {
+    const updatedPets = pets.map(p => {
+      if (p.id === pet.id) {
+        p.isAdopted = true
+        return p
+      } else {
+        return p
+      }
+    })
+    setPets(updatedPets)
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +43,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser onAdoptPet={onAdoptPet} pets={pets}/>
           </div>
         </div>
       </div>
@@ -27,3 +55,11 @@ function App() {
 }
 
 export default App;
+
+
+/**
+ * App
+ *  Filters
+ *  PetBrowser
+ *    Pet
+ */
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,11 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({onAdoptPet, pets}) {
+
+  const petComps = pets.map(pet => <Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet}/>)
+
+  return <div className="ui cards">{petComps}</div>;
 }
 
 export default PetBrowser;
